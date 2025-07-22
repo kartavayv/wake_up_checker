7288e9e1274861e7397eeee26089d424ffb06b1d
KARTAVAY <72963323+kartavayv@users.noreply.github.com>
2025-07-22 23:09:53 +0530

    soem files bro

---
+++ wake_up_checker.py
@@ -0,0 +1,46 @@
+import discord
+import asyncio
+import os
+from datetime import datetime
+
+TOKEN = "MTM5NzI1ODQzMjI4OTE4MTgxOA.GStd-z.0XwmdVd9kKIzJ5zGEa46bxxeaHRWjtlFZIRD_I"
+CHANNEL_ID = 1397256653396775156  
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+
+shame_messages = [
+    "I FAILED to wake up. Again. #DisciplineFailure",
+    "Another fake grind day. Slept through it. #Shame",
+    "Excuses 1, Me 0. #WeakMind",
+    "If I keep this up, I deserve to fail. #FixYourself",
+    "I lost to the alarm clock. #NoDiscipline"
+]
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+    
+    file_path = "C:\\Users\\HP\\Desktop\\YOUR STUFF\\PROGRAMMING\\PROJECTS\\WAKE ME UP\\wake_up.txt"
+    now = datetime.now()
+    deadline_hour = 10
+    deadline_minute = 30
+
+    if now.hour > deadline_hour or (now.hour == deadline_hour and now.minute >= deadline_minute):
+        try:
+            with open(file_path, "r") as f:
+                status = f.read().strip().lower()
+        except:
+            status = ""
+
+        if status != "awake":
+            channel = client.get_channel(CHANNEL_ID)
+            for msg in shame_messages:
+                await channel.send(msg)
+                await asyncio.sleep(5)
+        else:
+            print("✅ You woke up on time.")
+    await client.close()
+
+client.run(TOKEN)
